05b2450e00780163107cf3754c9104ef0e4246a4
Shane Liu <lx1326607292@gamil.com>
2023-03-16 01:03:02 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost',{useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error',(error)=> console.error(error))
+db.once('open',()=> console.log('Connected to Database '))
+
+app.listen(3000, () => console.log('Server Started'))
